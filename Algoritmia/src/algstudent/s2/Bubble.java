5975f452a3e4c10744bd26971b688d82f9d3a431
mikel <mikel@PORTA-MIKEL>
2021-03-01 23:18:10 +0100

    Session2

---
+++ Algoritmia/src/algstudent/s2/Bubble.java
@@ -0,0 +1,43 @@
+﻿package algstudent.s2;
+
+/* This program can be used to sort n elements with 
+ * a "bad" algorithm (quadratic). 
+ * It is the BUBBLE or DIRECT EXCHANGE */
+public class Bubble extends Vector {
+	public Bubble(int nElements) {
+		super(nElements);
+	}
+
+	@Override
+	public void sort() {
+		for(int i = 1; i < elements.length; i++) {
+			for(int j = elements.length -1; j >= i; j--) {
+				if(elements[j-1] > elements[j]) {
+					Vector.interchange(elements, j-1, j);
+				}
+			}
+		}
+	} 
+	
+	public void sortWithSentinel(int[] elements) {
+		int i = 1;
+		boolean hasChange = true;
+		
+		while(hasChange && (i < elements.length)) {
+			hasChange = false;
+			for(int j = elements.length -1; j >= i; j--) {
+				if(elements[j-1] > elements[j]) {
+					Vector.interchange(elements, j-1, j);
+					hasChange = true;
+				}
+			}
+			i++;
+		}
+	}
+	
+	@Override
+	public String getName() {
+		return "Bubble";
+	} 
+} 
+
---
+++ Algoritmia/src/algstudent/s2/Selection.java
@@ -0,0 +1,26 @@
+﻿package algstudent.s2;
+
+/* This program can be used to sort n elements with 
+ * a "bad" algorithm (quadratic). 
+ * It is the SELECTION */
+public class Selection extends Vector {
+	public Selection(int nElements) {
+		super(nElements);
+	}
+	
+	@Override
+	public void sort() {
+		int posMin;
+		
+		for(int i = 0; i < elements.length - 1; i++) {
+			posMin = Vector.findPosMin(elements, i); //O(n)
+			Vector.interchange(elements, i, posMin);
+		}
+	}  
+	
+	
+	@Override
+	public String getName() {
+		return "Selection";
+	} 
+} 
